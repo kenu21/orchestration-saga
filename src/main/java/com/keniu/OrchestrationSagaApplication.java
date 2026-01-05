f624ea663f340d596cf8d94b43a86e38a4c4897a
Yurii <yurij.kenu@gmail.com>
2026-01-05 15:05:32 +0100

    first commit

---
+++ src/main/java/com/keniu/OrchestrationSagaApplication.java
@@ -0,0 +1,13 @@
+package com.keniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestrationSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestrationSagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keniu/OrchestrationSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keniu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
